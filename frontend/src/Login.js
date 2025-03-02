fa767d4d365fb7863fe03c6141692066d9ca8f52
Alejandro García Menor <alejandro.garciamenor@usp.ceu.es>
2025-03-02 11:07:26 +0100

    Estilo pagina de login + estilos comunes

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./styles/Login.css";
+import "./styles/common.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -21,14 +23,19 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Iniciar sesión</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit">Iniciar sesión</button>
-      </form>
-      {error && <p>{error}</p>}
+    <div className="login-container">
+      <div className="login-image">
+        <img src={require("./images/login.jpg")} alt="Imagen de LogIn" />
+      </div>
+      <div className="login-form">
+        <h2>Iniciar sesión</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <button type="submit">Iniciar sesión</button>
+        </form>
+        {error && <p>{error}</p>}
+      </div>
     </div>
   );
 };
