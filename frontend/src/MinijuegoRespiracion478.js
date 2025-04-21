176ee98334ffbfcd62ac7c99ba76ab4363c66963
Alejandro García Menor <alejandro.garciamenor@usp.ceu.es>
2025-04-21 17:46:09 +0200

    estilos css para lograr movimiento circulo respiracion478

--- frontend/src/MinijuegoRespiracion478.js
+++ frontend/src/MinijuegoRespiracion478.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from "react";
+import './styles/MinijuegoRespiracion478.css';
+
 
 const pasos = [
     {nombre: "Inhala", duracion: 4000},
@@ -10,14 +12,16 @@ const MinijuegoRespiracion478 = () => {
     
     const [pasoIndex, setPasoIndex] = useState(0);
     const [esActivo, setEsActivo] = useState(false);
-
+    const [tipoCirculo, setTipoCirculo] = useState('');
     
     // temporizador para controlar el paso del juego
     useEffect(() => {
         let temporizador;
         if(esActivo){
             const pasoActual = pasos[pasoIndex];
-
+            if(pasoIndex === 0) setTipoCirculo('aumentar');
+            if(pasoIndex === 1) setTipoCirculo('mantener');
+            if(pasoIndex=== 2) setTipoCirculo('contraer');
 
             temporizador = setTimeout(() => {
                 const siguienteIndex = (pasoIndex + 1) % pasos.length;
@@ -34,7 +38,7 @@ const MinijuegoRespiracion478 = () => {
 
     return (
         <div className="minijuego-container">
-
+            <div className={`circulo-respiracion ${tipoCirculo}`}></div>
             <p className="texto-respiracion">{pasos[pasoIndex].nombre}</p>
             { !esActivo && (
                 <button className="boton-comenzar-minijuego" onClick={comenzarRespiracion}>Comenzar Minijuego</button>
