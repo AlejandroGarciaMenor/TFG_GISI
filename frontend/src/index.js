f32838cc7987f9b772cefbd18c7c9d10ae1c60f4
Alejandro García Menor <alejandro.garciamenor@usp.ceu.es>
2025-05-04 11:13:40 +0200

    eliminadas importaciones Chakra

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { system } from "@chakra-ui/react/preset";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
