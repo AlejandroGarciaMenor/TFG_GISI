ed6c1e9f71926cf78c691ddf7580ee9a673c0043
Ubuntu <ubuntu@ip-172-31-20-54.ec2.internal>
2025-06-22 10:19:27 +0000

    pequeños cambios para prueba login artillery

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,6 +29,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  max: 50,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 // Configuración del servicio de correo
